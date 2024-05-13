87fccdcb5929b9979ffb9ba6512067cbeb804557
Christian Reizner <christian.reizner@gmail.com>
2024-05-13 20:57:49 +0200

    PlanetaryFixation with public setter

--- DataModels/PlanetaryFixation.cs
+++ DataModels/PlanetaryFixation.cs
@@ -11,11 +11,11 @@ public sealed class PlanetaryFixation
     /// Gets the planets fixing state of the activation. Depending on the gate and line where the planet is situated, it can
     /// be in exaltation, detriment or both or no special state.
     /// </summary>
-    public FixingState FixingState { get; init; }
+    public FixingState FixingState { get; set; }
     
     /// <summary>
     /// If a fixing state changes due to a comparison set of celestial objects, such as transits or a partner chart, then this value is true.
     /// In the case of an individual chart, this means that if a fixing state of a personal planet is changed by a design planet (or vice versa), then this value is true.
     /// </summary>
-    public bool FixingStateChangedByComparer { get; init; }
+    public bool FixingStateChangedByComparer { get; set; }
 }
